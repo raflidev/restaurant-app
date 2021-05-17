f004063c99293f37c35fc841f13c9fa11bbca0f8
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-05-17 10:53:07 +0700

    favourite page

--- src/scripts/views/pages/favourite.js
+++ src/scripts/views/pages/favourite.js
@@ -1,12 +1,24 @@
+import favouriteResto from '../../data/favourite-resto';
+import { createListRestaurant } from '../templates/template-creator';
+
 const Favourite = {
   async render() {
     return `
-    <h1>page favourite</h1>
+    <h1 style='padding:20px;margin:0;text-align:center'>
+      Favourite Restaurantmu
+    </h1>
+    <div class="container-favourite">
+    </div>
     `;
   },
 
   async afterRender() {
     // Fungsi ini akan dipanggil setelah render()
+    const resto = await favouriteResto.getAllResto();
+    const container = document.querySelector('.container-favourite');
+    resto.forEach((data) => {
+      container.innerHTML += createListRestaurant(data);
+    });
   },
 };
 
