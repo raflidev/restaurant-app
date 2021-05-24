478bb5007b410150e68e3a2b0df70f0c488bd060
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-05-24 21:25:33 +0700

    add search feature

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import Home from '../views/pages/home';
 import Aboutus from '../views/pages/aboutus';
 import Favourite from '../views/pages/favourite';
 import Detail from '../views/pages/detail';
+import Search from '../views/pages/search';
 
 const routes = {
   '/': Home,
   '/about-us': Aboutus,
   '/detail/:id': Detail,
   '/favourite': Favourite,
+  '/search/:id': Search,
 };
 
 export default routes;
