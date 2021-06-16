c5659db6b734b27eb4383a82d4fd032d92a1848e
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-06-16 20:29:37 +0700

    add footer

--- src/scripts/views/pages/aboutus.js
+++ src/scripts/views/pages/aboutus.js
@@ -1,14 +1,17 @@
 const Aboutus = {
   async render() {
     return `
-    <div>
-      <div class="card">
-        <div class="card_image"></div>
-        <div class="card_description">
+    <div class="about_us_page">
+      <div class="card_about">  
+        <img class="card_about_image" src="https://www.gravatar.com/avatar/f57ab01833402ccd0f7daa934b68a572?s=200">
+        <div class="card_about_description">
           <span>Muhammad Rafli Ramadhan</span>
           <p>Frontend Web Developer</p>
         </div>
-        <div class="card_link">
+        <div class="card_about_link">
+          <a href="instagram.com/raflidev" target="_blank" >
+            <i class="fab fa-instagram width_svg"></i>
+          </a>
         </div>
       </div>
     </div>
