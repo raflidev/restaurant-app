442a9e8ca318900ecd853c2385946767e7606bc9
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-06-22 20:01:49 +0700

    fix submission 2

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -55,18 +55,18 @@ const createDetailRestaurant = (resto) => `
   <div class="detail-content">
     <div class="detail-page">
       <div class="image">
-        <img src="${config.imageEndpoint}/${resto.pictureId}" alt="${resto.name}">
+        <img tabindex="7" src="${config.imageEndpoint}/${resto.pictureId}" alt="${resto.name}">
       </div>
       <div class="resto-detail">
-        <h2>${resto.name}</h2>
+        <h2 tabindex="8">${resto.name}</h2>
         <span>${resto.rating} ${createRatingRestaurant(resto.rating)} </span>
         <p><i class="fa fa-map-marker-alt"></i>  ${resto.address} - ${resto.city}</p>  
-        <p>${resto.description}</p>
+        <p tabindex="9">${resto.description}</p>
 
 
         <div class="category">
           <span>Kategori : </span>
-          <div class="menu-resto">
+          <div class="menu-resto" tabindex="10">
             
           </div>
         </div>
@@ -74,31 +74,31 @@ const createDetailRestaurant = (resto) => `
     </div>
     <div class="menu-page">
       <h3>Daftar Makanan</h3>
-      <div class="food-section">
+      <div class="food-section" tabindex="11">
       </div>
 
       <h3>Daftar Minuman</h3>
-      <div class="drink-section">
+      <div class="drink-section" tabindex="12">
       </div>
     </div>
 
-    <div class="review-page">
+    <div class="review-page" tabindex="13">
       <h3>Apa kata mereka?</h3>
       <div class="wrapper-customer-review"></div>
     </div>
     
     <div class="post-review">
-      <h3>Ingin memberikan feedback kepada restoran ini?</h3>
+      <h3 tabindex="14">Ingin memberikan feedback kepada restoran ini?</h3>
       <form id="form-review">
         <div>
           <label>Name</label>
-          <input type="text" id="review-name" name="name" placeholder="Nama" />
+          <input tabindex="15" type="text" id="review-name" name="name" placeholder="Nama" />
         </div>
         <div>
           <label>Review</label>
-          <textarea name="review" id="review-text" row=3 col=2 placeholder="Review"></textarea>
+          <textarea tabindex="16" name="review" id="review-text" row=3 col=2 placeholder="Review"></textarea>
         </div>
-        <button id="review-submit" type="submit">Kirim Feedback</button>
+        <button tabindex="17" id="review-submit" type="submit">Kirim Feedback</button>
       </form>
     </div>
   </div>
