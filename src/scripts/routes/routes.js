7bdcd55c7c9a823a1ccd21c74b4c6a18a03cc93d
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-07-21 16:06:09 +0700

    finishing

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,10 @@
 import Home from '../views/pages/home';
-import Aboutus from '../views/pages/aboutus';
 import Favourite from '../views/pages/favourite';
 import Detail from '../views/pages/detail';
 import Search from '../views/pages/search';
 
 const routes = {
   '/': Home,
-  '/about-us': Aboutus,
   '/detail/:id': Detail,
   '/favourite': Favourite,
   '/search': Search,
